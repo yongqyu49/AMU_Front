bb245dcac2e682c6ecaca9ba9bee5394253b02eb
yongqyu49 <cyk728@naver.com>
2024-11-07 16:21:47 +0900

    Back에서 유저 데이터 받아오기 성공

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import axios from "axios";
 
 function App() {
   const [hello, setHello] = useState('');
+  const [user, setUser] = useState('');
 
-  useEffect(() => {
     axios.get('/api/test')
         .then((res) => {
-          setHello(res.data);
-        })
-  }, []);
+        setHello(res.data);
+    })
+  axios.post('/api/getOneUser')
+      .then((res) => {
+          setUser(res.data);
+      })
   return (
       <div className="App">
         백엔드 데이터 : {hello}
+          유저: {user.id}, {user.artist}
       </div>
   );
 }
